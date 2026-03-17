30309db5750c32e5ac3d740c7a4e71a575aa1a51
jules vincent taveau <jules.vincent--taveau@epfedu.fr>
2026-03-17 10:57:15 +0100

    version 4

--- zombie_level1_daylight_soviet_abandoned.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="zombie_level1_daylight_soviet_abandoned" tilewidth="500" tileheight="20" tilecount="244" columns="2">
- <image source="../../../Downloads/zombie_level1_daylight_soviet_abandoned.png" width="1360" height="2440"/>
-</tileset>
